4f91c25184150f1cce138d1b5fdf84fa843cfef8
Vinay <vinay.patilashok@gmail.com>
2020-02-15 14:51:31 +0000

    added multiplication method

---
+++ src/Addition.java
@@ -0,0 +1,17 @@
+
+public class Addition {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 10;
+		int b = 11;
+		int c;
+		
+		c=a+b;
+		System.out.println(c);
+		
+		
+	}
+
+}
